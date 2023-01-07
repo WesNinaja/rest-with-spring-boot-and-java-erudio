262b3a9012d4c9010a0ce3a11233b6576de0a2c2
wesley.calle <wesley.calle@idwall.co>
2023-01-07 18:51:43 -0300

    feat: Creating integration Test in controller to validate the application CORS

--- src/main/java/br/com/erudio/controller/PersonController.java
+++ src/main/java/br/com/erudio/controller/PersonController.java
@@ -46,6 +46,7 @@ public class PersonController {
         return service.findAll();
     }
 
+    @CrossOrigin(origins = "http://localhost:8080")
     @GetMapping(value = "{id}", produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
     //Annotation do swagger
     @Operation(summary = "Finds a Person", description = "Finds a Person",
@@ -64,6 +65,7 @@ public class PersonController {
         return service.findById(id);
     }
 
+//    @CrossOrigin(origins = {"http://localhost:8080", "https://erudio.com.br"})
     @PostMapping(consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML},
             produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
     //Annotation do swagger
