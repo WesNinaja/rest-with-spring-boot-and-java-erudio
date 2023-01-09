77e8296ceaff3829c81b65964b4c763a904839ee
wesley.calle <wesley.calle@idwall.co>
2023-01-08 21:12:55 -0300

    feat: Implementing Security config

--- src/main/java/br/com/erudio/Startup.java
+++ src/main/java/br/com/erudio/Startup.java
@@ -2,12 +2,34 @@ package br.com.erudio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class Startup {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Startup.class, args);
+
+		/*
+		 *Como faremos para gerar uma senha?
+		 * Com o trecho de código abaixo. Isso vai imprimir no console uma senha encriptada, no caso admin234
+		 */
+
+		Map<String, PasswordEncoder> encoders = new HashMap<>();
+		encoders.put("bcrypt", new BCryptPasswordEncoder());
+		DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("bcrypt", encoders);
+		passwordEncoder.setDefaultPasswordEncoderForMatches(new BCryptPasswordEncoder());
+
+		String result = passwordEncoder.encode("admin234");
+		System.out.println("My hash " + result);
 	}
+
+
+
 
 }
---
+++ src/main/java/br/com/erudio/config/SecurityConfig.java
@@ -0,0 +1,78 @@
+package br.com.erudio.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+
+import br.com.erudio.security.jwt.JwtConfigurer;
+import br.com.erudio.security.jwt.JwtTokenProvider;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Autowired
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public SecurityConfig(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    //Esse cara vai dizer pro spring como ele deve encriptar senhas pra poder fazer a comparação com a senha que está no banco
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        Map<String, PasswordEncoder> encoders = new HashMap<>();
+//        encoders.put("bcrypt", new BCryptPasswordEncoder());
+//        DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("bcrypt", encoders);
+//        passwordEncoder.setDefaultPasswordEncoderForMatches(new BCryptPasswordEncoder());
+//        return passwordEncoder;
+//    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http
+                .httpBasic().disable()//Aqui estamos desabilitando a autenticação Basic
+                .csrf(AbstractHttpConfigurer::disable) //or (csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) //Significa que a nossa sessão não vai armazenar estado, ela vai ser Stateless
+                .authorizeHttpRequests(
+                        //Aqui dizemos que é necessário autorização para cada requests.
+                        authorizeHttpRequests -> authorizeHttpRequests
+                                .requestMatchers("/auth/signin", "/auth/refresh", "/swagger-ui/**", "/v3/api-docs/**").permitAll() //Essas urls não precisam se autenticar
+                                .requestMatchers("/api/**").authenticated() //Somente autenticado
+                                .requestMatchers("/users").denyAll() //Aqui negamos as urls
+                )
+                .cors()
+                .and()
+                .apply(new JwtConfigurer(jwtTokenProvider))
+                .and()
+                .build();
+
+    }
+
+}
